70933c142f9459110f1066fbd3f471c4706feef6
Marieme Ngom <mngom@MacBook-Pro.local>
2021-08-04 21:27:50 -0500

    fixed bugs

--- code/vvkernels.py
+++ code/vvkernels.py
@@ -14,7 +14,7 @@ class TensorProductKernel(MultitaskKernel):
     Class to get the tensorproduct kernel
     """
 
-    def __init__(self, data_covar_module, chol_q,  num_tasks, rank=1, pos_constraint = None, tri_constaint = None, task_covar_prior=None, **kwargs):
+    def __init__(self, data_covar_module, chol_11, chol_21, chol_22,  num_tasks, rank=1, pos_constraint = None, tri_constaint = None, task_covar_prior=None, **kwargs):
         super().__init__(data_covar_module, num_tasks, rank, task_covar_prior, **kwargs)
 #        self.num_tasks = num_tasks
 #        self.rank = rank
@@ -25,7 +25,7 @@ class TensorProductKernel(MultitaskKernel):
         
         # register the raw parameter
         self.register_parameter(
-            name='chol_factor', parameter=torch.nn.Parameter(chol_q)
+            name='chol_11', parameter=torch.nn.Parameter(chol_11)
         )
 
         # set the parameter constraint to be triangular
@@ -33,23 +33,67 @@ class TensorProductKernel(MultitaskKernel):
             pos_constraint = Positive()
 
         # register the constraint
-        self.register_constraint("chol_factor", pos_constraint)
+        self.register_constraint("chol_11", pos_constraint)
         
         @property
-        def cholesky_factor(self):
-            return self.chol_factor_constraint.transform(self.chol_factor)
+        def cholesky_11(self):
+            return self.chol_11_constraint.transform(self.chol_11)
             
-        @cholesky_factor.setter
-        def cholesky_factor(self, value):
+        @cholesky_11.setter
+        def cholesky_11(self, value):
             return self._set_cholesky_factor(value)
             
         
-        def _set_cholesky_factor(self, value):
+        def _set_cholesky_11(self, value):
             if not torch.is_tensor(value):
-                value = torch.as_tensor(value).to(self.chol_factor)
+                value = torch.as_tensor(value).to(self.chol_11)
                 
-            self.initialize(chol_factor = self.chol_factor_constraint.inverse_transform(value))
+            self.initialize(chol_11 = self.chol_11_constraint.inverse_transform(value))
+            
+            
+            # register the raw parameter
+        self.register_parameter(
+            name='chol_22', parameter=torch.nn.Parameter(chol_22)
+        )
+
+
+        # register the constraint
+        self.register_constraint("chol_22", pos_constraint)
         
+        @property
+        def cholesky_22(self):
+            return self.chol_22_constraint.transform(self.chol_22)
+            
+        @cholesky_22.setter
+        def cholesky_22(self, value):
+            return self._set_cholesky_22(value)
+            
+        
+        def _set_cholesky_22(self, value):
+            if not torch.is_tensor(value):
+                value = torch.as_tensor(value).to(self.chol_22)
+                
+            self.initialize(chol_22= self.chol_22_constraint.inverse_transform(value))
+        
+                    # register the raw parameter
+        self.register_parameter(
+            name='chol_21', parameter=torch.nn.Parameter(chol_21)
+        )
+        
+        @property
+        def cholesky_21(self):
+            return (self.chol_21)
+            
+        @cholesky_22.setter
+        def cholesky_21(self, value):
+            return self._set_cholesky_21(value)
+            
+        
+        def _set_cholesky_21(self, value):
+            if not torch.is_tensor(value):
+                value = torch.as_tensor(value).to(self.chol_21)
+                
+            self.initialize(chol_22= (value))
 
         
         
@@ -59,14 +103,19 @@ class TensorProductKernel(MultitaskKernel):
     def forward(self, x1, x2, diag=False, last_dim_is_batch=False, **params):
         k = self.data_covar_module
        # chol_factor = self.chol_q
-        kappa = self.chol_factor.mul(self.chol_factor.t())
+        chol_factor = torch.zeros(self.num_tasks, self.num_tasks)
+        chol_factor[0,0] = self.chol_11
+        chol_factor[1,1] = self.chol_22
+        chol_factor[1,0] = self.chol_21
+    
+        kappa = gpytorch.matmul(chol_factor, chol_factor.t())
+      
         x1 = x1.reshape(x1.shape[0], 1)
         x2 = x2.reshape(x2.shape[0], 1)
-        d1 = x1.shape[0]
-        d2 = x2.shape[0]
-        kronecker_kernel = torch.zeros(self.num_tasks * d1, self.num_tasks*d2)
+
         k_fwd = k.forward(x1, x2)
         kronecker_kernel = torch.kron(k_fwd, kappa)
+        return kronecker_kernel
 #
 #        for i in range(self.num_tasks):
 #            for j in range(self.num_tasks):
@@ -75,11 +124,14 @@ class TensorProductKernel(MultitaskKernel):
 #                kappa_ij = kappa[i,j]
 #                kronecker_kernel[i,j] = torch.kron(k_fwd, kappa_ij)
             
-        
-        return kronecker_kernel #(kronecker_kernel.reshape(self.num_tasks * d1, self.num_tasks*d2))
+        #print(kronecker_kernel)
+              #  kronecker_kernel = torch.zeros(self.num_tasks * d1, self.num_tasks * d2)
+#        print(kappa)
+         #(kronecker_kernel.reshape(self.num_tasks * d1, self.num_tasks*d2))
         
 
-
+#        d1 = x1.shape[0]
+#        d2 = x2.shape[0]
 
         
         
--- code/vvlikelihood.py
+++ code/vvlikelihood.py
@@ -32,21 +32,22 @@ class TensorProductLikelihood(MultitaskGaussianLikelihood):
         self.cov_noise2 = _cov_noise2
 
 
-    def get_hpll(self, out_data, g_theta1,mu, K):
+    def get_hpll(self, out_data, g_theta1, mu, K):
 #        g_theta1 = g_theta(theta1)
         
         C11 = K.forward(g_theta1, g_theta1.t())
+      #  print(torch.det(C11))
         out_data = out_data.reshape(C11.shape[1], 1)
         m0 = mu.forward(g_theta1)
         m0 = m0.reshape(out_data.shape[0], 1)
 #        one_vec = torch.ones(C11.shape[1], 1)
-#        den = gpytorch.inv_quad(C11, one_vec)
+#        den = gpytorch.inv_matmul(C11, one_vec, one_vec.t())
 #        num = gpytorch.inv_matmul(C11, out_data, one_vec.t())
-#
+
 #        m0 = (num/den) * one_vec
         
         
-        inv_quad_C11 = gpytorch.inv_quad(C11, out_data - m0)
+        inv_quad_C11 = gpytorch.inv_matmul(C11, out_data - m0, (out_data - m0).t())
         logdet_C11 = gpytorch.logdet(C11)
         return -.5 * logdet_C11 - .5 * inv_quad_C11
         
@@ -59,8 +60,8 @@ class TensorProductLikelihood(MultitaskGaussianLikelihood):
 
         """
 
-#        g_theta1 = g_theta(theta1)
-#        g_theta2 = g_theta(theta2)
+#       g_theta1 = g_theta(theta1)
+#       g_theta2 = g_theta(theta2)
         cov_noise1 = self.cov_noise1
         cov_noise2 = self.cov_noise2
 
@@ -71,11 +72,11 @@ class TensorProductLikelihood(MultitaskGaussianLikelihood):
         Cf1 = K.forward(x,g_theta1.t())
         Cf2 = K.forward(x, g_theta2.t())
         C11 = K.forward(g_theta1, g_theta1.t()) #+ cov_noise1
-
         C12 = K.forward(g_theta1, g_theta2.t())
+
         C22 = K.forward(g_theta2, g_theta2.t()) #+ cov_noise2
 
-        mean_data = mu.forward(agg_data)
+        mean_data = mu.forward(g_theta1)
         mean_data = mean_data.reshape(mean_data.shape[0] * mean_data.shape[1], 1)
         agg_data = agg_data.reshape(mean_data.shape[0], 1)
         rhs_g1 =  agg_data- mean_data #g-S
@@ -88,28 +89,26 @@ class TensorProductLikelihood(MultitaskGaussianLikelihood):
         pf1 = mean_x + gpytorch.inv_matmul(C11, rhs_g1, Cf1)
 
         C21 = C12.t()
-        print(C22.shape)
-        Q21 = C22 - gpytorch.inv_quad(C11, C12)
-
+        Q21 = C22 - gpytorch.inv_matmul(C11, C12, C12.t())
         C1f = Cf1.t()
-        Qf1 = Cff - gpytorch.inv_quad(C11, C1f) #
+        Qf1 = Cff - gpytorch.inv_matmul(C11, C1f, C1f.t()) #
 
 
         C2f = Cf2.t()
         second_term_Qf12 = C2f - gpytorch.inv_matmul(C11,C1f, C21)
-      #  t_second_term_Qf12 = second_term_Qf12.t()
-    
 
-        Qf12_sec_term = gpytorch.inv_quad(Q21, second_term_Qf12)
+
+        Qf12_sec_term = gpytorch.inv_matmul(Q21, second_term_Qf12, second_term_Qf12.t())
         Qf12 = Qf1 - Qf12_sec_term
-
-
-        inv_quad_Qf12 = gpytorch.inv_quad(Qf12, f_target - pf1)
+        
+        
+        
+        inv_quad_Qf12 = gpytorch.inv_matmul(Qf12,  f_target - pf1, (f_target - pf1).t())
         logdet_Qf12 = gpytorch.logdet(Qf12)
         # simplified trace expression
         trace_term_arg = gpytorch.inv_matmul(Qf12,Qf1)
         diag_trace_term_arg = trace_term_arg.diag()
-        trace_term = diag_trace_term_arg .sum()
+        trace_term = diag_trace_term_arg.sum()
 
 
 
